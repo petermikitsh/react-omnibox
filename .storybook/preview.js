463368759b265dba98b04c5743a749868fb3cfd5
Peter Mikitsh <peter.mikitsh@gmail.com>
2021-10-27 19:43:48 -0700

    chore: add storybook dark mode

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,29 @@
+import React from 'react';
+import { DocsContainer as BaseContainer } from '@storybook/addon-docs/blocks';
+import { useDarkMode } from 'storybook-dark-mode';
+import { themes } from '@storybook/theming';
+
+export const DocsContainer = ({ children, context }) => {
+  const dark = useDarkMode();
+
+  return (
+    <BaseContainer
+      context={{
+        ...context,
+        parameters: {
+          ...context.parameters,
+          docs: {
+            // This is where the magic happens.
+            theme: dark ? themes.dark : themes.light
+          },
+        },
+      }}
+    >
+      {children}
+    </BaseContainer>
+  );
+}
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -5,5 +31,9 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  // https://github.com/hipstersmoothie/storybook-dark-mode/issues/127#issuecomment-802018811
+  docs: {
+    container: DocsContainer,
   },
 }
