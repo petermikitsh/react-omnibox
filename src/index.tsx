55e91f18c7670884bdb1d1816dbd1dceaf363b47
Peter Mikitsh <peter.mikitsh@gmail.com>
2021-10-27 19:27:47 -0700

    chore: remove scaffold files

---
+++ src/index.tsx
@@ -0,0 +1,3 @@
+export const Omnibox = () => {
+  return null;
+};
--- stories/Page.stories.tsx
+++ stories/Page.stories.tsx
@@ -1,23 +1,14 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-
-import { Page } from './Page';
-import * as HeaderStories from './Header.stories';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Omnibox } from "../src";
 
 export default {
-  title: 'Example/Page',
-  component: Page,
-} as ComponentMeta<typeof Page>;
+  title: "Example/Page",
+  component: Omnibox,
+} as ComponentMeta<typeof Omnibox>;
 
-const Template: ComponentStory<typeof Page> = (args) => <Page {...args} />;
+const Template: ComponentStory<typeof Omnibox> = (args) => (
+  <Omnibox {...args} />
+);
 
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-  // More on composing args: https://storybook.js.org/docs/react/writing-stories/args#args-composition
-  ...HeaderStories.LoggedIn.args,
-};
-
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {
-  ...HeaderStories.LoggedOut.args,
-};
+export const Example = Template.bind({});
